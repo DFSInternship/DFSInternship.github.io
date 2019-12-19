c1c71f0b5ca836b6b0ddfc0848536e69694e135c
JFcoolGuyExtraordinaire <45076527+JFcoolGuyExtraordinaire@users.noreply.github.com>
2019-12-19 11:13:41 -0500

    footerRemove

---
+++ resources/removeFoot.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function(){
+  var foot = document.getElementsByClassName('formFooter-heightMask')[0];
+  foot.remove();
+  foot = document.getElementsByClassName('formFooter')[0];
+  foot.remove();
+});
